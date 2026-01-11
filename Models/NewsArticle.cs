0d848d5f1bad775e09fc2258e09d055d9cd2f9e8
the3dit0r <76208541+The3dit0r@users.noreply.github.com>
2026-01-11 12:48:00 +0700

    Added news displaying component, layout change for home and article details page

---
+++ Models/NewsArticle.cs
@@ -0,0 +1,18 @@
+﻿namespace NewsManagement.Models;
+public class NewsArticle
+{
+    public DateTime? CreatedDate { get; set; }
+
+    public string NewsArticleId { get; set; } = string.Empty;
+    public string NewsTitle { get; set; } = string.Empty;
+    public string Headline { get; set; } = string.Empty;
+
+    public string NewsContent { get; set; } = string.Empty;
+    public string NewsSource { get; set; } = string.Empty;
+    public string CategoryID { get; set; } = string.Empty;
+    public string NewsStatus { get; set; } = string.Empty;
+    public string CreatedByID { get; set; } = string.Empty;
+    public string UpdatedByID { get; set; } = string.Empty;
+    public string ModifiedDate { get; set; } = string.Empty;
+
+}
